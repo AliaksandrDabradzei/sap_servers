bd6205df1ece2499bf78ed584f25d3bdadb335db
Alexander Dobrodey <Alexander.Dobrodey@gmail.com>
2013-05-17 10:16:33 +0300

    new models

--- sap_servers/models.py
+++ sap_servers/models.py
@@ -2,68 +2,117 @@ from django.db import models
 
 # Create your models here.
 class OS(models.Model):
-    name = models.CharField(max_length=35) # Windows
-    bit = models.IntegerField(default=64) 
-    
+    name = models.CharField(max_length=25, default="WS2008 R2 SE")  # Windows 2008 or others
+    bit = models.IntegerField(default=64)  # 32x or 64
+
     def __unicode__(self):
-        return u'%s %s' % (self.name, self.bit)
+        return u'%s x%s' % (self.name, self.bit)
     
 class Database(models.Model):
-    name = models.CharField(max_length=30) #Oracle
+    name = models.CharField(max_length=20, default="Oracle")  # e.g. Oracle
     version = models.CharField(max_length=20) #11.2.0.3
+
+    def __unicode__(self):
+        return self.name
+    
+class Locations(models.Model):
+    location = models.CharField(max_length=20)  # e.g. K1
     
     def __unicode__(self):
-        return u'%s %s' % (self.name, self.version)
-    
-class Location(models.Model):
-    location = models.CharField(max_length=20) # K1
-    
-    def __unicode__(self):
-        return self.location   
-    
-class Host(models.Model):    
-    name = models.CharField(max_length=20) # evbyminsd1270
-    isVirtual = models.BooleanField(default=True) # V/H
-    location = models.ForeignKey(Location)
-    SBEA = models.BooleanField(default=False)
+        return self.location
+
+class Hosts(models.Model):    
+    name = models.CharField(max_length=20)  # e.g. evbyminsd1904
+    isVirtual = models.BooleanField()  # Virtual or Hard?
+    location = models.ForeignKey(Locations)  # e.g. K1-3
+    SBEA = models.BooleanField()
     OS = models.ForeignKey(OS)
-    RAM = models.IntegerField(default=0)
-    HDD_all = models.IntegerField(default=0)
-    HDD_occup = models.IntegerField(default=0)
+    RAM = models.IntegerField(default=0)  # in GB
+    HDD_all = models.IntegerField(default=0)  # in GB
+    HDD_occup = models.IntegerField(default=0)  # in GB
     database = models.ForeignKey(Database)
+
+    def __unicode__(self):
+        return self.name
+
+class Projects(models.Model):
+    name = models.CharField(max_length=10)  # e.g. EPMS-PRTS
+
+    def __unicode__(self):
+        return self.name
+
+class Products(models.Model):
+    name = models.CharField(max_length=30)  # e.g. SAP NetWeaver
+    version = models.CharField(max_length=20)  # e.g. 7.0
+
+    def __unicode__(self):
+        return self.name
+
+class Landscapes(models.Model):
+    name = models.CharField(max_length=20)  # e.g. Production
+
+    def __unicode__(self):
+        return self.name
+
+class SystemStatus(models.Model):
+    status = models.CharField(max_length=20)  # e.g. switched-off
+
+    def __unicode__(self):
+        return self.status
+
+class SystemOwners(models.Model):
+    first_name = models.CharField(max_length=20) #Aliaksandr
+    last_name = models.CharField(max_length=20)  #Dabradzei    
+    email = models.EmailField(blank=True)   #Aliaksandr_Dabradzei@epam.com
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
+class InstanceType(models.Model):
+    type = models.CharField(max_length=20)  # ABAP, JAVA, ABAP+JAVA, BO
+
+    def __unicode__(self):
+        return self.type
+
+class Instance(models.Model):
+    sid = models.CharField(max_length=10) # SM7
+    instance_nr = models.IntegerField() # 00
+    instance_type = models.ForeignKey(InstanceType) 
+    hosts = models.ManyToManyField(Hosts)
+
+    def __unicode__(self):
+        return self.sid
+    
+class Licenses(models.Model):
+    license = models.IntegerField()
+    license_exp = models.DateField()
+    
+    def __unicode__(self):
+        return self.license
+    
+class Systems(models.Model):
+    name = models.CharField(max_length=20)  # e.g. evbyminsd1904_pi5
+    pool = models.CharField(max_length=30)  # e.g. SAP Servers
+    isOnline = models.BooleanField(default=False)  # e.g. offline
+    status = models.ForeignKey(SystemStatus)
+    landscape = models.ForeignKey(Landscapes)
+    projects = models.ManyToManyField(Projects)
+    instance = models.ForeignKey(Instance)
+    product = models.ManyToManyField(Products)
+    specification = models.CharField(max_length=100)  # e.g. based on NW7.0 EHP1
+    uc = models.BooleanField(default=False)
+    clients = models.CharField(max_length=100, default="000, 001,")  # e.g. 000, 001, 100
+    owner = models.ForeignKey(SystemOwners)  # e.g. "Aleh Mikhniuk"
+    license = models.ForeignKey(Licenses)
+    HWU = models.IntegerField()
+    HWU_exp = models.DateField()
     
     def __unicode__(self):
         return self.name
 
-# 
-# class Instance(models.Model):
-#     sid = models.CharField(max_length=10)
-#     instance_nr = models.DecimalField()
-#     instance_type = models.CharField(max_length=20)
-#     
-# class Projects(models.Model):
-#     name = models.CharField(max_length=10)
-#     
-# class Products(models.Model):
-#     name = models.CharField(max_length=30)
-#     version = models.CharField(max_length=20)
-#     
-# class Servers(models.Model):
-#     name = models.CharField(max_length=20)
-#     pool = models.CharField(max_length=30)
-#     host = models.ManyToManyField(Hosts)
-#     status = models.CharField(max_length=15)
-#     landscape = models.CharField(max_length=20)
-#     projects = models.ManyToManyField(Projects)
-#     instance = models.ManyToManyField(Instance)
-#     product = models.ManyToManyField(Products)
-#     specification = models.CharField(max_length=100)
-#     uc = models.BooleanField(default=False)
-#     clients = models.CharField(max_length=30)
-#     owner = models.CharField(max_length=60)
-#     license = models.DecimalField()
-#     license_exp = models.DateField()
-#     HWU = models.DecimalField()
-#     HWU_exp = models.DateField()
-#     
-#     
+
+
+
+
+
+    
