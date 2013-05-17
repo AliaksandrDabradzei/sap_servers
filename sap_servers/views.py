b08caa4aaecd3cc27a903dd1ba827fbae5dfb93d
Alexander Dobrodey <Alexander.Dobrodey@gmail.com>
2013-05-17 13:23:10 -0430

    ALL_hosts.html

--- sap_servers/views.py
+++ sap_servers/views.py
@@ -4,4 +4,4 @@ from sap_servers.models import Host
 
 def hosts_detail(request):
     m_list = Host.objects.all()
-    return render_to_response('template1.html', {'m_list': m_list})
+    return render_to_response('All_hosts.html', {'m_list': m_list})
