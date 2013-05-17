49d0f25c06b5df2b4b7526c7ce56458ce66c4f5e
Alexander Dobrodey <Alexander.Dobrodey@gmail.com>
2013-05-17 07:13:11 -0430

    returned

--- sap_servers/loadTables.py
+++ sap_servers/loadTables.py
@@ -4,13 +4,22 @@ Created on May 17, 2013
 @author: Aliaksandr_Dabradzei
 '''
 from xlrd import open_workbook, cellname
-from sap_servers.models import *
+# from models import *
+from SAPServers.models import OS
+
 
 sheet = open_workbook('d:\PROGRAMMING\servers.xls').sheet_by_index(0)
 sids = []
 # for row in range(sheet.nrows):
 #     sids.append(str(sheet))
-print sheet.col_values(0)
+oses = list(set(sheet.col_values(5, 1)))
+for os in oses:
+    os = os.strip()
+    if os[0] == 'x':
+        bit = os[1, 3]
+        print bit
+#     p = OS(
+#            )
 
 
 
@@ -30,14 +39,14 @@ print sheet.col_values(0)
 #         
 #     print values
 
-#sheet = wb.sheet_by_index(0)
+# sheet = wb.sheet_by_index(0)
 
-#print sheet.name
+# print sheet.name
 
 # for row_index in range(1):
 #     for col_index in range(sheet.ncols):
 #         print cellname(row_index, col_index), '-',
 #         print sheet.cell(row_index, col_index).value
 
-#print sheet.row_values(0,1)
-#print sheet.col(1)
+# print sheet.row_values(0,1)
+# print sheet.col(1)
